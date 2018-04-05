6007e724e5b8189822ba233d766a7e3daecbded8
gaojie <472781184@qq.com>
2018-04-05 13:09:31 +0800

    first commit

---
+++ src/main/java/com/jk51/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.jk51.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+}
